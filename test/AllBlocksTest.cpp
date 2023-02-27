d7556609263addfe43e1d74247f05982d7b91905
mgurga <motiurga@gmail.com>
2023-02-26 20:57:34 -0800

    added new test: "AllBlocksTest"
    this test has all scratch blocks in one file, so we can start
    parsing blocks. currently tests fail, but fix coming soon.

---
+++ test/AllBlocksTest.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "../src/Project.hpp"
+#include "../src/FileHandler.hpp"
+
+class AllBlocksTest : public ::testing::Test {
+protected:
+    static std::string* temp_dir;
+    static Project* project;
+
+    static void SetUpTestSuite() {
+        temp_dir = new std::string("allblocks/");
+        FileHandler sb3 = FileHandler("allblocks.sb3", *temp_dir);
+        sb3.init([&] () {
+            std::cout << "finished unzipping " << sb3.file_name_no_ext << std::endl;
+        });
+
+        project = new Project(*temp_dir);
+        project->load_from_project_json(false);
+    }
+
+    static void TearDownTestSuite() {
+        std::cout << "deleteing " << *temp_dir << " temp directory" << std::endl;
+        std::filesystem::remove_all(*temp_dir);
+        delete project;
+        delete temp_dir;
+    }
+};
+
+Project* AllBlocksTest::project = nullptr;
+std::string* AllBlocksTest::temp_dir = nullptr;
+
+TEST_F(AllBlocksTest, MetaTest) {
+    ASSERT_EQ(project->meta.semver, "3.0.0");
+}
