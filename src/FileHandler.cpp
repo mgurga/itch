7e2090411b8a56af6683b09663beab5c6b50f6b7
mgurga <motiurga@gmail.com>
2023-04-29 16:17:37 -0700

    use zip library instead of 7zip hack

--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -1,15 +1,26 @@
 #include "FileHandler.hpp"
 
-FileHandler::FileHandler(std::filesystem::path fn, std::filesystem::path temp_dir):
+FileHandler::FileHandler(std::filesystem::path fn, std::filesystem::path temp_dir) :
     file_name(fn),
     temp_dir(temp_dir),
-    file_name_no_ext(fn.string().substr(0, fn.string().find_last_of(".")))
-{}
+    file_name_no_ext(fn.string().substr(0, fn.string().find_last_of("."))) {}
 
 void FileHandler::init(std::function<void()> callback) {
     std::cout << "initalizing sb3 file" << std::endl;
 
-    int szout = system(std::string("7z x " + file_name.string() + " -o" + temp_dir.string()).c_str());
+    // int szout = system(std::string("7z x " + file_name.string() + " -o" +
+    // temp_dir.string()).c_str());
+
+    auto on_extract_entry = [](const char *filename, void *arg) {
+        static int i = 0;
+        int n = *(int *)arg;
+        printf("Extracted: %s (%d of %d)\n", filename, ++i, n);
+
+        return 0;
+    };
+
+    int arg = 2;
+    zip_extract(file_name.string().c_str(), temp_dir.string().c_str(), on_extract_entry, &arg);
 
     initialized = true;
     callback();
--- src/FileHandler.hpp
+++ src/FileHandler.hpp
@@ -1,10 +1,12 @@
 #pragma once
-#include <string>
-#include <functional>
+#include <zip.h>
+
 #include <filesystem>
+#include <fstream>
+#include <functional>
 #include <iostream>
 #include <nlohmann/json.hpp>
-#include <fstream>
+#include <string>
 
 using json = nlohmann::json;
 
@@ -12,7 +14,7 @@ class FileHandler {
 public:
     FileHandler(std::filesystem::path file_path, std::filesystem::path temp_dir);
     bool initialized = false;
-    std::filesystem::path file_name; // .sb3 filename
+    std::filesystem::path file_name;  // .sb3 filename
     std::string file_name_no_ext;
     std::filesystem::path temp_dir;
 
