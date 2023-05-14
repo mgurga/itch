a1ff0e919689bda062c2ae4cb504a2f464bcff24
mgurga <motiurga@gmail.com>
2023-05-14 15:28:10 -0700

    replace variable refrences with get/set

--- src/scratch/ScratchStage.hpp
+++ src/scratch/ScratchStage.hpp
@@ -11,10 +11,14 @@ public:
     ScratchStage();
     ScratchStage(json ss, std::filesystem::path temp_dir, bool heavyload = true);
 
-    int& tempo() { return m_tempo; }
-    VideoState& videoState() { return m_videoState; }
-    int& videoTransparency() { return m_videoTransparency; }
-    std::string& textToSpeechLanguage() { return m_textToSpeechLanguage; }
+    int get_tempo() { return m_tempo; }
+    void set_tempo(int t) { m_tempo = t; }
+    VideoState get_video_state() { return m_videoState; }
+    void set_video_state(VideoState s) { m_videoState = s; }
+    int get_video_transparency() { return m_videoTransparency; }
+    void set_video_transparency(int v) { m_videoTransparency = v; }
+    virtual std::string get_tts_language() { return m_textToSpeechLanguage; }
+    virtual void set_tts_language(std::string t) { m_textToSpeechLanguage = t; }
 
 private:
     int m_tempo;
