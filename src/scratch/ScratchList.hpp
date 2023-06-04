c32f7d80d33adc04229dc305b05b5fd70dc24a0f
mgurga <motiurga@gmail.com>
2023-06-03 22:07:44 -0700

    convert non-string values to string
    this commit stops the json library from adding quotes to a string when
    using dump().

--- src/scratch/ScratchList.hpp
+++ src/scratch/ScratchList.hpp
@@ -9,7 +9,12 @@ using json = nlohmann::json;
 class ScratchList {
 public:
     ScratchList(json sv, std::string id) : name(sv[0].get<std::string>()), id(id) {
-        for (auto v : sv[1]) { values.push_back(v.dump()); }
+        for (auto v : sv[1]) {
+            if (v.type() == nlohmann::detail::value_t::string)
+                values.push_back(v.get<std::string>());
+            else
+                values.push_back(v.dump());
+        }
     };
 
     std::string name;
