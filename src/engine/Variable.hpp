3d9c6384076723102a467491d1929e630df12157
mgurga <motiurga@gmail.com>
2023-07-02 23:54:54 -0700

    take variable value as a Value instead of a string

--- src/engine/Variable.hpp
+++ src/engine/Variable.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <string>
-#include <vector>
-#include <optional>
 #include <nlohmann/json.hpp>
+#include <optional>
+#include <string>
 #include <variant>
+#include <vector>
 
 #include "../scratch/ScratchVariable.hpp"
 #include "Value.hpp"
@@ -13,24 +13,21 @@ using json = nlohmann::json;
 
 class Variable {
 public:
-    Variable(std::string name, std::string value, std::string id = ""):
-        name(name), value(value), id(id), is_global(true), is_cloud(false)
-    {};
-    Variable(json sv, std::string id = ""):
+    Variable(std::string name, Value value, std::string id = "") :
+        name(name), value(value), id(id), is_global(true), is_cloud(false){};
+    Variable(json sv, std::string id = "") :
         name(to_string(sv[0])),
         value(to_string(sv[1])),
         is_cloud(sv.size() == 3),
         is_global(true),
-        id(id)
-    {};
-    Variable(const ScratchVariable& sv):
-        name(sv.name), value(sv.value), is_cloud(sv.isCloud), is_global(true), id(sv.id)
-    {};
+        id(id){};
+    Variable(const ScratchVariable& sv) :
+        name(sv.name), value(sv.value), is_cloud(sv.isCloud), is_global(true), id(sv.id){};
 
     std::string name;
     bool is_cloud;
-    bool is_global; // otherwise only available in target
-    std::string sprite_name; // if is_global is false this is sent to the sprite name
+    bool is_global;           // otherwise only available in target
+    std::string sprite_name;  // if is_global is false this is sent to the sprite name
 
     void make_local(std::string sprite_name) {
         is_global = false;
@@ -45,9 +42,7 @@ public:
         }
     }
 
-    Value val() {
-        return value;
-    }
+    Value val() { return value; }
 
     Variable operator=(const std::string& other) {
         value = other;
@@ -78,6 +73,7 @@ public:
         value += other;
         return *this;
     }
+
 private:
     std::string id;
     Value value;
